83d058857663aaff69f2bf5e3fed6d29bd1e4655
Josep Quintana <josep.quintana.44@gmail.com>
2020-10-01 00:12:35 +0200

    Final version

---
+++ 1. Chatty/src/server.erl
@@ -0,0 +1,29 @@
+-module(server).
+%% Exported Functions
+-export([start/0]).
+
+start() ->
+    ServerPid = spawn(fun() -> process_requests([]) end),
+    register(myserver, ServerPid).
+
+process_requests(Clients) ->
+    receive
+        {client_join_req, Name, From} ->
+            NewClients = [From|Clients],
+            broadcast(NewClients, {join, Name}),
+            process_requests(NewClients);
+        {client_leave_req, Name, From} ->
+            NewClients = lists:delete(From, Clients),
+            broadcast(Clients, {leave, Name}),
+            From ! exit,
+            process_requests(NewClients);
+        {send, Name, Text} ->
+            broadcast(Clients, {message, Name, Text}),
+            process_requests(Clients);
+        disconnect ->
+            unregister(myserver)
+    end.
+
+broadcast(PeerList, Message) ->
+    Fun = fun(Peer) -> Peer ! Message end,
+    lists:map(Fun, PeerList).
